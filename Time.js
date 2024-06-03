6e472017f83f270def3b618353256f70180bf74a
Aquti <103832857+Aquti@users.noreply.github.com>
2024-06-03 09:51:01 +0200

    Create Time.js

---
+++ Time.js
@@ -0,0 +1,17 @@
+{
+    "abbreviation": "UTC",
+    "client_ip": "217.55.247.252",
+    "datetime": "2023-06-03T07:26:37.833827+00:00",
+    "day_of_week": 1,
+    "day_of_year": 155,
+    "dst": false,
+    "dst_from": null,
+    "dst_offset": 0,
+    "dst_until": null,
+    "raw_offset": 0,
+    "timezone": "Etc/UTC",
+    "unixtime": 1685770384,
+    "utc_datetime": "2023-06-03T07:26:37.833827+00:00",
+    "utc_offset": "+00:00",
+    "week_number": 23
+}
